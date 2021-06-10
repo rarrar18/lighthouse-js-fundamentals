fd3b2759f2cc0904349b61663aab271899959847
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-10 08:14:01 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,4 @@
+//checking if separate conditions are true
 const raining = false;
 const cold = false;
 
@@ -9,6 +10,7 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
+//checking if a variable falls within the conditions
 const temperature = 16;
 
 if (temperature < 0) {
@@ -20,3 +22,21 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//checking if either condition is true
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//checking if a condition is false
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+//checking if both conditions are true
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
