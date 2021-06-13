396f7e93bf8d36ff5a669e10ef71c3c9c28cd18d
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-13 10:24:39 +0000

    Changes to hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,14 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
-sayHello();
+sayHello(); //prints out Hello undefined
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//using a return value instead of solely console.log
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
