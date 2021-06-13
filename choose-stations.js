fd677cdb735690c62cda5824bb7f5b9e353e8b3d
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-13 10:55:34 +0000

    Add choose-stations.js to dir

---
+++ choose-stations.js
@@ -0,0 +1,28 @@
+//good stations have at least 20 person capacity
+//stations can be schools or community centres
+
+const stations =[
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+]
+
+function chooseStations (stations){
+  const goodStations = []
+
+  for (const station of stations) {
+    const capacity = station[1]
+
+    if (capacity >= 20){
+      
+      const type = station[2]
+
+      if (type === "school" || type === "community centre"){
+        goodStations.push(station[0])
+      }
+    }
+  }
+  return goodStations
+}
+
+console.log(chooseStations(stations));
