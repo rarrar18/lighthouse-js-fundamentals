f9ba8a287cb525493d9f55f4cf0ed94bd51fb79b
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-13 11:30:03 +0000

    Add how-many-hundreds.js to dir

---
+++ how-many-hundreds.js
@@ -0,0 +1,12 @@
+function howManyHundreds(num){
+  return parseInt(num / 100);
+}
+//parseInt function should return the number without fractions
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
+
+//runs functions, expected answers on the right
